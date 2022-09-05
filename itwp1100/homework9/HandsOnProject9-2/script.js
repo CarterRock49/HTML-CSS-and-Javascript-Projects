8123afea4e83d273093916722a8b1608c78e01c5
Carter Rock <84819233+CarterRock49@users.noreply.github.com>
2022-09-05 14:13:37 -0400

    Add files via upload

---
+++ itwp1100/homework9/HandsOnProject9-2/script.js
@@ -0,0 +1,50 @@
+/*
+JavaScript 6th Edition
+Chapter 9
+Hands-on Project 9-2
+
+Author: Carter Rock
+Date:   4/18/2021
+
+Filename: script.js
+*/
+"use strict";
+function processCookie(){
+	document.cookie = "username=" + document.getElementById("usernameinput").value;
+}
+
+function populateInfo(){
+	if (document.cookie) {
+		var uname = document.cookie;
+		uname = uname.substring(uname.lastIndexOf("=") + 1);
+		document.getElementById("usernameinput").value = uname;
+	}
+}
+
+function handleSubmit(){
+	 if (evt.preventDefault) {
+		evt.preventDefault();
+	 } else {
+		evt.returnValue = false;
+	 }
+	 processCookie();
+	 document.getElementsByTagName("form")[0].submit();
+}
+
+function createEventListener(){
+	var loginForm = document.getElementsByTagName("form")[0];
+	if (loginForm.addEventListener) {
+		loginForm.addEventListener("submit", handleSubmit, false);
+	} else if (loginForm.attachEvent) {
+		loginForm.attachEvent("onsubmit", handleSubmit); 
+	}
+}
+function setUpPage() {    
+	populateInfo();
+	createEventListener();
+}
+if (window.addEventListener) {
+	window.addEventListener("load", setUpPage, false);16   
+}   else if (window.attachEvent) {
+	window.attachEvent("onload", setUpPage);
+}
